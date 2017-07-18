b057f7236de02237424bd0ca538e1c25881f962a
sunilquadros <sunilquadros@hotmail.com>
2017-07-18 15:32:55 -0500

    Update file.php

--- file.php
+++ file.php
@@ -1,5 +1,10 @@
 <?php
+//$servername = "172.30.251.114";
+//$username = "ose";
+//$password = "openshift";
+//$dbname = "quotes";
 // Create connection
+//$conn = new mysqli($servername, $username, $password, $dbname);
 $conn = mysqli_connect($_ENV["MYSQLDB_SERVICE_HOST"],"instructor","openshift","instructor",
  $_ENV["MYSQLDB_SERVICE_PORT"])
      or die("Error " . mysqli_error($conn));
@@ -15,23 +20,14 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
    // output data of each row
    while($row = $result->fetch_assoc()) {
-        $instructorNumber = $row["instructorNumber"];
-        $instructorName = $row["instructorName"];
-        $email = $row["email"];
-        $city = $row["city"];
-        $state = $row["state"];
-        $postalCode = $row["postalCode"];
-        $country = $row["country"];
-      echo "  <div style='margin:30px 0px;'>
-        Instructor Number: $instructorNumber<br />
-        Instructor Name:  $instructorName<br />
-        Email: $email<br />
-        City: $city<br />
-        State: $state<br />
-        Postal Code: $postalCode<br />
-        Country: $country<br />
-      </div>
-      ";   
+          echo "InNum:- " . $row["instructorNumber"]. " " ;
+          echo "InName:- " . $row["instructorName"]. " " ;
+          echo "Email:- " . $row["email"]. " " ;
+          echo "City:- " . $row["city"]. " " ;
+          echo "State:- " .  $row["state"]. " " ;
+          echo "PostalCode:- " . $row["postalCode"]. " " ;
+          echo "Country:- " . $row["country"]. " " ;
+        echo " \n";
    }
 } else {
    echo "0 results";
